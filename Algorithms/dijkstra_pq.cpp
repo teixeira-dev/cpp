8ce07a28df456513bc0e33028e5741cf22c59505
teixeira-dev <matos.teixeira10@gmail.com>
2024-08-15 20:39:24 -0300

    Added dijkstra's algorithm with priority queue
    ...

---
+++ Algorithms/dijkstra_pq.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void unique_subarrays(vector<int> a, vector<int> b, int i = 0){
+    for(auto it: b) cout << it << " ";
+    cout << endl;
+    for(int ind = i; ind < a.size(); ind++){
+        if(ind > i && a[ind] == a[ind - 1]) continue;
+        b.push_back(a[ind]);
+        unique_subarrays(a, b, ind + 1);
+        b.pop_back();
+    }
+}
+
+int main(){
+    vector<pair<int, int>> a[6];
+    a[0] = {{1, 4}, {2, 4}};
+    a[1] = {{0, 4}, {2, 2}};
+    a[2] = {{0, 4}, {1, 2}, {3, 3}, {5, 6}, {4, 1}};
+    a[3] = {{2, 3}, {5, 2}};
+    a[4] = {{2, 1}, {5, 3}};
+    a[5] = {{3, 2}, {2, 6}, {4, 3}};
+
+    int dist[6];
+    fill(begin(dist), end(dist), INT_MAX);
+    dist[0] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({dist[0], 0});
+    
+    int front, dis;
+    while(!pq.empty()){
+        front = pq.top().second;
+        dis = pq.top().first;
+        pq.pop();
+        if(dist[front] != dis) continue;
+        for(auto it: a[front]){
+            if(dis + it.second < dist[it.first]){
+                dist[it.first] = dis + it.second;
+                pq.push({dist[it.first], it.first});
+            }
+        }
+    }
+
+    for(int i = 0; i < 6; i++)
+        cout << i << ": > " << dist[i] << endl;
+}
